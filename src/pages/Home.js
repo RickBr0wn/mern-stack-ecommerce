afa5195950be87f60fa2d929c3146bc7b423eac9
Rick Brown <ricky.brown.00@gmail.com>
2022-01-16 12:56:07 +0000

    adds navbar to home page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar'
+import Message from '../components/Message'
+import Slider from '../components/Slider'
+
+const Home = () => {
+	return (
+		<div>
+			<Message />
+			<Navbar />
+			<Slider />
+		</div>
+	)
+}
+
+export default Home

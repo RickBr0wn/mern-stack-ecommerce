f87403f4d1a21efe627078168356d6d75041142b
Rick Brown <ricky.brown.00@gmail.com>
2022-01-18 11:03:00 +0000

    adds more media-queries (commented)

--- src/responsive.js
+++ src/responsive.js
@@ -1,9 +1,26 @@
 import { css } from 'styled-components'
 
-export const mobile = props => {
-	return css`
-		@media only screen and (max-width: 380px) {
-			${props}
-		}
-	`
-}
+export const mediaQuery = props => css`
+	@media only screen and (max-width: 380px) {
+		${props}
+	}
+`
+// TODO: During design utilise these media query functions
+
+// export const desktop = props => css`
+// 	@media screen and (min-width: 1024px) {
+// 		${props}
+// 	}
+// `
+
+// export const tablet = props => css`
+// 	@media screen and (min-width: 768px) and (max-width: 1023px) {
+// 		${props}
+// 	}
+// `
+
+// export const mobile = props => css`
+// 	@media screen and (max-width: 767px) {
+// 		${props}
+// 	}
+// `
